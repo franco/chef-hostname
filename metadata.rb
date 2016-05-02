450ed7ae9235af5b4954d761f6844c1f2092bddb
Franco Sebregondi <me@usershell.org>
2016-05-02 17:49:51 +0400

    Add name to metadata
    Chef-client v12 introduced a breaking change. From version 12 on,
    every cookbook requires a name attribute in their metadata file.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
+name              "hostname"
 maintainer        "Nickolay Kovalev"
 maintainer_email  "nickola@nickola.ru"
 license           "MIT"
 description       "Sets hostname and FQDN of the node"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0"
+version           "1.1"
 
 %w{ubuntu debian}.each do |os|
   supports os
